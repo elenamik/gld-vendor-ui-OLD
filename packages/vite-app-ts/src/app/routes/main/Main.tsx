cae87ead7deaefdba298fdfde0d50abe3fed6af4
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-06-14 19:42:19 -0400

    can read tokens + eth in vendor

--- packages/vite-app-ts/src/app/routes/main/Main.tsx
+++ packages/vite-app-ts/src/app/routes/main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useEthersContext } from 'eth-hooks/context';
 
 import '~~/styles/main-page.css';
@@ -11,26 +11,46 @@ import { formatEther } from '@ethersproject/units';
 
 export const Main: FC = () => {
     const scaffoldAppProviders = useScaffoldProviders();
-
     const ethersContext = useEthersContext();
-
     const appContractConfig = useAppContracts();
 
-    const contract = useContractLoader(appContractConfig, undefined, 4)
-    const vendorContract = contract['Vendor'] as any;
-    console.log('VENDOR', vendorContract)
-    const vendorBal = useBalance(vendorContract?.address)
-    console.log(formatEther(BigNumber.from(vendorBal)))
 
+    const readContracts = useContractLoader(appContractConfig)
+    const vendorContract = readContracts['Vendor'] as any;
+    const tokenContract = readContracts['YourToken'] as any;
+
+
+    const [vendorTokenBal, setVendorTokenBal] = useState<string >('0')
+    const vendorEthBal = parseEther(useBalance(vendorContract?.address))
+
+    const readTokenVendorBal = async ()=> {
+        const getVendorTokenBal = async ()=> {
+            const bal = await tokenContract?.balanceOf(vendorContract?.address)
+            setVendorTokenBal(parseEther(bal))
+        }
+        await getVendorTokenBal()
+    }
+    useEffect(()=> {
+        if (vendorContract){
+            readTokenVendorBal()
+        }
+    }, [vendorContract?.address])
 
     return (
     <div className="App">
       hi, world
     <div>
-        balance of ETH in Vendor is {} ETH
+        balance of ETH in Vendor is {vendorEthBal} ETH
     </div>
+        <div>
+            balance of tokens in Vendor is {vendorTokenBal} ETH
+        </div>
     </div>
   );
 };
 
 export default Main;
+
+export const parseEther= (balance: BigNumber)=> {
+    return formatEther(BigNumber.from(balance));
+}
