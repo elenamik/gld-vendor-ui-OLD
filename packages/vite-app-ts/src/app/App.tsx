b8a7e1fef6ee62a2b96b33a9357e7436e341fe24
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-07-03 17:09:38 -0400

    some cleanup

--- packages/vite-app-ts/src/app/App.tsx
+++ packages/vite-app-ts/src/app/App.tsx
@@ -1,11 +1,8 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { EthersAppContext } from 'eth-hooks/context';
 import React, { FC, lazy, Suspense } from 'react';
 import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
+
 import { ErrorBoundary, ErrorFallback } from '~~/app/common/ErrorFallback';
-import { BLOCKNATIVE_DAPPID } from '~~/models/constants/constants';
-import { subgraphUri } from '~~/config/subgraphConfig';
-import { EthersAppContext } from 'eth-hooks/context';
-import { EthComponentsSettingsContext, IEthComponentsSettings } from 'eth-components/models';
 
 /**
  * See MainPage.tsx for main app component
@@ -23,23 +20,10 @@ import '~~/styles/css/app.css';
 // load saved theme
 const savedTheme = window.localStorage.getItem('theme');
 
-//setup themes for theme switcher
+// setup themes for theme switcher
 const themes = {
   dark: './dark-theme.css',
   light: './light-theme.css',
-};
-
-// load graphql client for subgraphs
-const client = new ApolloClient({
-  uri: subgraphUri,
-  cache: new InMemoryCache(),
-});
-
-// create eth components context for options and API keys
-const context: IEthComponentsSettings = {
-  apiKeys: {
-    BlocknativeDappId: BLOCKNATIVE_DAPPID,
-  },
 };
 
 /**
@@ -52,19 +36,15 @@ const App: FC = () => {
   console.log('loading app...');
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <ApolloProvider client={client}>
-        <EthComponentsSettingsContext.Provider value={context}>
-          <EthersAppContext>
-            <ErrorBoundary FallbackComponent={ErrorFallback}>
-              <ThemeSwitcherProvider themeMap={themes} defaultTheme={savedTheme || 'light'}>
-                <Suspense fallback={<div />}>
-                  <MainPage />
-                </Suspense>
-              </ThemeSwitcherProvider>
-            </ErrorBoundary>
-          </EthersAppContext>
-        </EthComponentsSettingsContext.Provider>
-      </ApolloProvider>
+      <EthersAppContext>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <ThemeSwitcherProvider themeMap={themes} defaultTheme={savedTheme || 'light'}>
+            <Suspense fallback={<div />}>
+              <MainPage />
+            </Suspense>
+          </ThemeSwitcherProvider>
+        </ErrorBoundary>
+      </EthersAppContext>
     </ErrorBoundary>
   );
 };
